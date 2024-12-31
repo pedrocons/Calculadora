9f03ffe5cdd00912f6184e94a134488429adb94f
Pedro Henrique Pereira Prates <pedrohprates@hotmail.com>
2024-12-31 16:16:27 -0300

    Primeiro commit

---
+++ src/components/Button.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { StyleSheet, Text, Button, Dimensions, TouchableHighlight, } from "react-native";
+
+export default props => {
+
+    return (
+        <TouchableHighlight onPress={props.onClick} >
+            <Text style={styles.buttom} >
+                {props.label}
+            </Text>
+        </TouchableHighlight>
+
+    )
+}
+
+const styles = StyleSheet.create({
+    buttom: {
+        fontSize: 40,
+        height: Dimensions.get('window').width / 4,
+        width: Dimensions.get('window').width / 4,
+        padding: 20,
+        backgroundColor: '#f0f0f0',
+        textAlign: 'center',
+        borderWidth: 1,
+        borderColor: '#888'
+    }
+})
